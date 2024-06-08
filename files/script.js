93a3240fe036a240a286df0b9a1e085706ee0a53
pasamsrikanth <pasam01134@gmail.com>
2024-06-08 15:53:50 +0530

    framework_add

--- files/script.js
+++ files/script.js
@@ -1,6 +1,6 @@
 
 var typed = new Typed(".text", {
-    strings: ["Python Development" , "Django" , "Web Development"],
+    strings: ["Python Development" , "Django Framework" , "Web Development"],
     typeSpeed:100,
     backSpeed:100,
     backDelay:1000,
